e5372e5cfd33cf3ac19341879b6afa588b150c6e
jklmnlkevin <zhukewen_java@126.com>
2013-10-17 09:42:50 +0800

    first version

---
+++ src/com/daxia/lovedog/MainServlet.java
@@ -0,0 +1,21 @@
+package com.daxia.lovedog;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+
+	/**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+    	//
+    }
+}
